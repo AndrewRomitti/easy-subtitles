92e13643f218fc2b8fb720718629bd8cf6d0db1f
AndrewRomitti <andrewromitti@icloud.com>
2025-06-17 14:17:16 -0500

    Add margins

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from utils import *
 from moviepy import CompositeVideoClip, VideoFileClip, AudioFileClip
 
-def create_subtitles(audio_path, video, output="output/main.mp4"):
+def create_subtitles(audio_path, video, font_path, output="output/main.mp4"):
     audio = AudioFileClip(audio_path)
     video = VideoFileClip(video).subclipped(0, audio.duration)
 
     transcribed_text = get_transcribed_text(audio_path)
-    text_clips = get_text_clips(transcribed_text, video, "C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\font\\static\\Montserrat-ExtraBold.ttf")
+    text_clips = get_text_clips(transcribed_text, video, font_path)
 
 
     main_clip = CompositeVideoClip([video] + text_clips).with_audio(audio)
     main_clip.write_videofile(output)
 
 if __name__ == "__main__":
-    create_subtitles(audio_path="C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\audioGen\\audios\\genTextExaggerated.wav", video="C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\MinecraftVids.mp4")
+    create_subtitles(audio_path="C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\audioGen\\audios\\genTextExaggerated.wav", video="C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\MinecraftVids.mp4", font_path="C:\\Users\\15732\\Desktop\\Projects\\reel-maker\\font\\static\\Montserrat-ExtraBold.ttf")
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import whisper
 from moviepy import TextClip
+from moviepy.video.fx import Margin
 
 def get_transcribed_text(filename):
     audio = whisper.load_audio(filename)
@@ -11,27 +12,34 @@ def get_transcribed_text(filename):
 def get_text_clips(text, clip, font_path, delay=0):
     text_clip_array = []
 
+    margin = Margin(20)
+
     segments = text["segments"]
     for segment in segments:
         for word in segment["words"]:
             if word["start"] < delay:
                 continue
 
-            start = word["start"]
-            end = word["end"]
-            text_clip_array.append(
-                TextClip(
-                    text=word["word"],
-                    font_size=70,
-                    stroke_color="black",
-                    stroke_width=5,
-                    color="white",
-                    font=font_path,
-                    method="caption",
-                    size=(clip.w, clip.h),
-                ).with_start(start)
-                .with_end(end)
-                .with_position("center")
+            start, end, w = word["start"], word["end"], word["word"]
+
+            txt = TextClip(
+                text=w,
+                font_size=70,
+                stroke_color="black",
+                stroke_width=5,
+                color="white",
+                font=font_path,
+                method="label",
             )
+
+            txt = Margin(left=20, right=20, top=20, bottom=20, color=(0, 0, 0), opacity=0).apply(txt)
+
+            txt = (txt
+                   .with_start(start)
+                   .with_end(end)
+                   .with_position(("center", "bottom"))
+                  )
+
+            text_clip_array.append(txt)
 
     return text_clip_array
